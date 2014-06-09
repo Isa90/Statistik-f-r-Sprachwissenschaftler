5005cb1becdf4f01660ced58be1d5737653f36a9
Isa90 <Kochi@students.uni-marburg.de>
2014-06-09 22:31:10 +0200

    Name/Datum geändert

--- Kochi/hausaufgabe15.R
+++ Kochi/hausaufgabe15.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 15
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-06-04
+# Isabell Koch <Kochi@students.uni-marburg.de>
+# 2014-06-08
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 
