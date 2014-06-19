628df998552419ce6a8b2427a3d51aeb264b332f
Isa90 <Kochi@students.uni-marburg.de>
2014-06-19 14:37:20 +0200

    Name/Datum geändert

--- Kochi/hausaufgabe18.R
+++ Kochi/hausaufgabe18.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 18
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-06-17
+# Isabell Koch <Kochi@students.uni-marburg.de>
+# 2014-06-19
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 
